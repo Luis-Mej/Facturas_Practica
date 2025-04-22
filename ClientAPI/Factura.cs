83d4798b67086fbd37737a78d545013c68e108d4
Luis-Mej <xxluis.angel2001@gmail.com>
2025-04-22 03:06:36 -0500

    Avanzando

--- ClientAPI/Factura.cs
+++ ClientAPI/Factura.cs
@@ -30,7 +30,69 @@ namespace ClientAPI
         {
             dgvDetalles.Columns.Clear();
             dgvDetalles.DataSource = null;
+            dgvDetalles.AutoGenerateColumns = false;
+
+            dgvDetalles.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Id",
+                HeaderText = "IdProducto",
+                Name = "IdProducto",
+                Visible = false
+            });
+
+            dgvDetalles.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "NombreProducto",
+                HeaderText = "Nombre del Producto",
+                Name = "NombreProducto",
+                ReadOnly = true
+            });
+
+            dgvDetalles.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "PrecioUnitario",
+                HeaderText = "Precio Unitario",
+                Name = "PrecioUnitario",
+                ReadOnly = true
+            });
+
+            dgvDetalles.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Cantidad",
+                HeaderText = "Cantidad",
+                Name = "Cantidad",
+                ReadOnly = true
+            });
+
             dgvDetalles.DataSource = productoCarrito;
+
+            CalcularTotales();
+        }
+
+        private void CalcularTotales()
+        {
+            decimal totalSubtotal = 0;
+            decimal totalIVA = 0;
+            decimal totalGeneral = 0;
+
+            foreach (var item in productoCarrito)
+            {
+
+                decimal precio = item.PrecioUnitario;
+                int cantidad = item.Cantidad ?? 0;
+
+                decimal subtotal = precio * cantidad ;
+                decimal iva = subtotal * 0.15M;
+                decimal total = subtotal + iva;
+
+                totalSubtotal += subtotal;
+                totalIVA += iva;
+                totalGeneral += total;
+            }
+
+            txtSubTotal.Text = totalSubtotal.ToString("0.00");
+            txtIva.Text = totalIVA.ToString("0.00");
+            txtTotal.Text = totalGeneral.ToString("0.00");
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
