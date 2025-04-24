014bab02ee098035e529b5c39c1d13540121d0f9
viamatica <viamatica@VMT-LMEJIA>
2025-04-24 14:42:13 -0500

    TESTEANDO

--- Facturacion.Shared.Dto/FacturasDTOS/FactCabeceraDTO.cs
+++ Facturacion.Shared.Dto/FacturasDTOS/FactCabeceraDTO.cs
@@ -12,7 +12,7 @@ namespace Dtos.FacturasDTOS
         {
         }
 
-        public FactCabeceraDTO(string nombreCliente, string identificacion, string telefono, string email, DateTime fechaCreacion, decimal subTotal, decimal iva, decimal total, string nombreUsuario)
+        public FactCabeceraDTO(string nombreCliente, string identificacion, string telefono, string email, DateTime? fechaCreacion, decimal subTotal, decimal? iva, decimal? total, string nombreUsuario)
         {
             NombreCliente = nombreCliente;
             Identificacion = identificacion;
@@ -29,10 +29,10 @@ namespace Dtos.FacturasDTOS
         public string Identificacion { get; set; } = string.Empty;
         public string Telefono { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
         public decimal SubTotal { get; set; }
-        public decimal Iva { get; set; }
-        public decimal Total { get; set; }
+        public decimal? Iva { get; set; }
+        public decimal? Total { get; set; }
         public string NombreUsuario { get; set; } = string.Empty;
     }
 }
